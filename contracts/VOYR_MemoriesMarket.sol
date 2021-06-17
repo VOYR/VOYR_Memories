7079176d33da5977bf3b3de8f98028a123dcca52
drgorillamd <dr_gorilla@protonmail.com>
2021-06-17 14:43:20 +0200

    Final / untested yet

--- contracts/VOYR_MemoriesMarket.sol
+++ contracts/VOYR_MemoriesMarket.sol
@@ -19,8 +19,9 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
       event New_listing(address sender, uint256 token_id);
       event New_bid(address sender, uint256 amount);
       event Auction_closed(address seller, address buyer, uint256 token_id, uint256 final_price);
+      event Transfer(address to, uint256 value);
 
-      uint8creator_fee;
+      uint8 creator_fee;
 
       struct auction {
         address seller;
@@ -44,13 +45,12 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
 
       //@dev front-end will have to check isApprovedForAll(sender, marketplace address)
       //and setApprovalForAll(marketplace_addres, true)
-      function newAuction(uint256 _token_id, uint256 _min_price, uint256 _deadline, ) public {
-
-        auction current_new = auction()
+      function newAuction(uint256 _token_id, uint256 _min_price, uint256 _deadline) external {
+        auction current_new;
+        require(_current.deadline == 0, "Ongoing auction");
         current_new.seller = msg.sender;
         current_new.min_price = _min_price;
         current_new.deadline = _deadline;
-
 
         current_auctions[_token_id] = current_new;
         listed_tokens.push(_token_id);
@@ -60,9 +60,9 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
       }
 
 
-      function newBid(uint256 _token_id) public payable {
+      function newBid(uint256 _token_id) external payable {
         auction _current = current_auctions[_token_id];
-        require(_current.min_price != 0, "No corresponding auction");
+        require(_current.deadline != 0, "No corresponding auction");
         require(_current.min_price < msg.value && _current.highest_bid < msg.value, "place a higher bid");
         require(_current.deadline >= block.timestamp, "auction expired");
 
@@ -77,9 +77,9 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
       }
 
 
-      function closeSale(uint256 _token_id) public {
+      function closeSale(uint256 _token_id) external {
         auction _current = current_auctions[_token_id];
-        require(_current.min_price != 0, "no corresponding auction");
+        require(_current.deadline != 0, "no corresponding auction");
         require(_current.deadline <= block.timestamp, "auction still running");
 
         uint256 _creator = VOYR_Memories.creator(_token_id);
@@ -89,22 +89,21 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
 
         for (uint i=0; i< listed_tokens.length; i++) { //O(n) :(
             if (listed_tokens[i] == _token_id) {
-                listed_tokens[i] = listed_tokens[listed_tokens.length-1]; //no order needed -> swap&delete
+                listed_tokens[i] = listed_tokens[listed_tokens.length-1];
                 listed_tokens[listed_tokens.length-1].pop();
                 break;
+            }
         }
-
 
         VOYR_Memories.safeTransfer(_current.highest_bidder, token_id);
         safeTransfer(_current.seller, _current.highest_bid.sub(_creator_fee));
         safeTransfer(_creator, _creator_fee);
 
         emit Auction_closed(_current.seller, _current.highest_bidder, _token_id, _current._highest_bid);
-
       }
 
 
-      //@dev will be free if called by user - watchout for gas from other contract
+      //@dev 0 gas if called by user - watchout for gas from other contract
       function auctionsBySellers(address seller) external view returns(uint256[] memory){
         uint256[] memory auctions_by_seller = new uint256[]
 
@@ -126,13 +125,18 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
         return current_auctions[_token_id];
       }
 
-      function setCreatorFeePercent(uint8 _new_rate) public onlyOwner {
+      function setCreatorFeePercent(uint8 _new_rate) external onlyOwner {
         creator_fee = _new_rate;
       }
 
       fallback () external payable {  //fallback. Why are you so generous anyway?
          revert () ;}
 
-      function safeTransfer
+       //@dev taken from uniswapV2 TransferHelper lib
+       function safeTransferETH(address to, uint value) internal {
+           (bool success,) = to.call{value:value}(new bytes(0));
+           require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+           emit Transfer(to, value);
+       }
 
 }
