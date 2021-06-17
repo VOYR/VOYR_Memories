3714a7391dd70d0475d7108961f3e7174b3a75b1
drgorillamd <dr_gorilla@protonmail.com>
2021-06-17 17:48:42 +0200

    at least, it compiles

--- contracts/Memories_factory.sol
+++ contracts/Memories_factory.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL
 pragma solidity ^0.8.0;
 
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VOYRMemories is ERC721, Ownable {
 
@@ -14,18 +14,19 @@ contract VOYRMemories is ERC721, Ownable {
     mapping (uint256 => address) private _creator;
 
     string public contract_URI_shop;
+    string public _base_uri;
 
     constructor() ERC721("VOYRMemories", "VMEMO") {
-        _setBaseURI("");
         //see this example of contract_URI, some platform support returning a fee to originator
-        contract_URI_shop = "https://ipfs.io/ipfs/Qmbqp5hr2i3ug14V7EyR9PYjCSzKnQ272NyT4pTcqkTrGs";
+        contract_URI_shop = ""; //example : https://ipfs.io/ipfs/Qmbqp5hr2i3ug14V7EyR9PYjCSzKnQ272NyT4pTcqkTrGs
+        _base_uri = "";
         }
 
     function mintNFT(address receiver, string memory new_URI) external {
         _tokenIds.increment();
         _mint(receiver, _tokenIds.current());
         _setTokenURI(_tokenIds.current(), new_URI);
-        creator[_tokenIds.current()] = msg.sender;
+        _creator[_tokenIds.current()] = msg.sender;
     }
 
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
@@ -33,11 +34,11 @@ contract VOYRMemories is ERC721, Ownable {
         _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function tokenURI(uint256 token_id) external view override returns (string memory) {
+    function tokenURI(uint256 token_id) public view override returns (string memory) {
         require(_exists(token_id), "ERC721Metadata: nonexistent token");
-        return abi.encodePacked(_baseURI, _tokenURIs[tokenId]);
+        return string(abi.encodePacked(_base_uri, _tokenURIs[token_id]));
     }
-    
+
     function creator(uint256 token_id) external view returns (address) {
         require(_exists(token_id), "ERC721Metadata: nonexistent token");
         return _creator[token_id];
@@ -48,7 +49,7 @@ contract VOYRMemories is ERC721, Ownable {
     }
 
     function setBaseURI(string memory new_base) external onlyOwner {
-        _setBaseURI(new_base);
+        _base_uri = new_base;
     }
 
     function contractURI() public view returns (string memory) {
--- contracts/VOYR_MemoriesMarket.sol
+++ contracts/VOYR_MemoriesMarket.sol
@@ -3,18 +3,19 @@
 pragma solidity ^0.8.0;
 
 
-import "@OpenZeppelin/contracts/access/Ownable.sol";
-import "@OpenZeppelin/contracts/math/SafeMath.sol";
-import "@OpenZeppelin/contracts/token/ERC721/IERC721Enumerable.sol"; //implem by default in ERC721 - yeh, weird
-import "@OpenZeppelin/contracts/token/ERC721/ERC721Holder.sol";
-import "@OpenZeppelin/contracts/introspection/ERC165.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol"; //implem by default in ERC721 - yeh, weird
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "./Memories_factory.sol";
 
 
 
-contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
+
+contract VOYRMarketplace is Ownable, IERC721Receiver {
 
       using SafeMath for uint256;
-      IERC721Enumerable VOYR_Memories;
+      VOYRMemories VOYR_Memories;
 
       event New_listing(address sender, uint256 token_id);
       event New_bid(address sender, uint256 amount);
@@ -32,22 +33,20 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
       }
 
       mapping(uint256 => auction) private current_auctions; //token_id -> Auction
-      uint256 private listed_tokens;
+      uint256[] private listed_tokens;
 
       //@dev will only support VOYR_Memories NFTs -> this can be upgraded in future
       //release if needed
       constructor(address VOYR_Memories_address) {
-          _registerInterface(IERC721Receiver.onERC721Received.selector);
-          VOYR_Memories = IERC721Enumerable(VOYR_Memories_address);
+          VOYR_Memories = VOYRMemories(VOYR_Memories_address);
           creator_fee = 1; //in %
       }
-
 
       //@dev front-end will have to check isApprovedForAll(sender, marketplace address)
       //and setApprovalForAll(marketplace_addres, true)
       function newAuction(uint256 _token_id, uint256 _min_price, uint256 _deadline) external {
-        auction current_new;
-        require(_current.deadline == 0, "Ongoing auction");
+        require(current_auctions[_token_id].deadline == 0, "Ongoing auction");
+        auction memory current_new;
         current_new.seller = msg.sender;
         current_new.min_price = _min_price;
         current_new.deadline = _deadline;
@@ -55,13 +54,13 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
         current_auctions[_token_id] = current_new;
         listed_tokens.push(_token_id);
 
-        VOYR_Memories.safeTransferFrom(msg.sender, address(this), token_id);
+        VOYR_Memories.safeTransferFrom(msg.sender, address(this), _token_id);
         emit New_listing(msg.sender, _token_id);
       }
 
 
       function newBid(uint256 _token_id) external payable {
-        auction _current = current_auctions[_token_id];
+        auction memory _current = current_auctions[_token_id];
         require(_current.deadline != 0, "No corresponding auction");
         require(_current.min_price < msg.value && _current.highest_bid < msg.value, "place a higher bid");
         require(_current.deadline >= block.timestamp, "auction expired");
@@ -70,7 +69,7 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
         uint256 prev_bid = _current.highest_bid;
 
         current_auctions[_token_id].highest_bid = msg.value;
-        current_auctions[_token_id].highest.bidder = msg.sender;
+        current_auctions[_token_id].highest_bidder = msg.sender;
 
         safeTransfer(prev_bidder, prev_bid);
         emit New_bid(msg.sender, msg.value);
@@ -78,49 +77,57 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
 
 
       function closeSale(uint256 _token_id) external {
-        auction _current = current_auctions[_token_id];
+        auction memory _current = current_auctions[_token_id];
         require(_current.deadline != 0, "no corresponding auction");
         require(_current.deadline <= block.timestamp, "auction still running");
 
-        uint256 _creator = VOYR_Memories.creator(_token_id);
+        address _creator = VOYR_Memories.creator(_token_id);
         uint256 _creator_fee = _current.highest_bid.mul(creator_fee).div(100);
 
         delete current_auctions[_token_id];
 
-        for (uint i=0; i< listed_tokens.length; i++) { //O(n) :(
-            if (listed_tokens[i] == _token_id) {
-                listed_tokens[i] = listed_tokens[listed_tokens.length-1];
-                listed_tokens[listed_tokens.length-1].pop();
+        uint256[] memory _listed_tokens = listed_tokens;
+        for (uint i=0; i<_listed_tokens.length; i++) { //O(n) :(
+            if (_listed_tokens[i] == _token_id) {
+                _listed_tokens[i] = _listed_tokens[_listed_tokens.length-1];
                 break;
             }
         }
+        listed_tokens = _listed_tokens;
+        listed_tokens.pop();
 
-        VOYR_Memories.safeTransfer(_current.highest_bidder, token_id);
+        VOYR_Memories.safeTransferFrom(address(this), _current.highest_bidder, _token_id);
         safeTransfer(_current.seller, _current.highest_bid.sub(_creator_fee));
         safeTransfer(_creator, _creator_fee);
 
-        emit Auction_closed(_current.seller, _current.highest_bidder, _token_id, _current._highest_bid);
+        emit Auction_closed(_current.seller, _current.highest_bidder, _token_id, _current.highest_bid);
       }
 
 
-      //@dev 0 gas if called by user - watchout for gas from other contract
       function auctionsBySellers(address seller) external view returns(uint256[] memory){
-        uint256[] memory auctions_by_seller = new uint256[]
+        uint256[] memory auctions_by_seller = new uint256[](0);
 
         for(uint256 i = 0; i<listed_tokens.length; i++) {
             if(current_auctions[i].seller == msg.sender) {
-              auctions_by_seller.push(i);
+              uint256[] memory tmp = new uint256[](auctions_by_seller.length+1);
+
+              for(uint j = 0; j<auctions_by_seller.length+1; j++) {
+                tmp[j] = auctions_by_seller[j];
+              }
+
+              tmp[tmp.length - 1] = i;
+              auctions_by_seller = tmp;
             }
         }
         return auctions_by_seller;
       }
 
 
-      function allAuctions() external view returns(uint256[]) {
+      function allAuctions() external view returns(uint256[] memory) {
         return listed_tokens;
       }
 
-      function auctionsDetails(uint256 _token_id) external view returns(uint256) {
+      function auctionsDetails(uint256 _token_id) external view returns(auction memory) {
         require(current_auctions[_token_id].min_price != 0, "No current auction");
         return current_auctions[_token_id];
       }
@@ -130,10 +137,15 @@ contract VOYR_Marketplace is Ownable, IERC721Receiver, ERC165, ERC721Holder {
       }
 
       fallback () external payable {  //fallback. Why are you so generous anyway?
-         revert () ;}
+         revert ();
+      }
+
+      function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes memory _data) public override returns(bytes4) {
+        return 0x150b7a02;
+      }
 
        //@dev taken from uniswapV2 TransferHelper lib
-       function safeTransferETH(address to, uint value) internal {
+       function safeTransfer(address to, uint value) internal {
            (bool success,) = to.call{value:value}(new bytes(0));
            require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
            emit Transfer(to, value);
